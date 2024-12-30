7fb46791a4d67706a0cd1218f14328deaf0760a5
InvestUz <invest.abdusattorov@gmail.com>
2024-12-30 12:35:53 +0500

    text dart has been added

---
+++ lib/utils/text.dart
@@ -0,0 +1,12 @@
+class AppText {
+  static final enText = {
+    'welcome_text': 'Welcome',
+    'signIn_text': 'Sign in to your account',
+    'registered_text': 'Already have an account',
+    'register_text':
+        'You can easly sign up, and connect to the Doctors nearb you',
+    'signUp_text': 'don\'t have an account?',
+    'social_login': 'Or continue with social account',
+    'forgot_password': 'Forgot your password',
+  };
+}
