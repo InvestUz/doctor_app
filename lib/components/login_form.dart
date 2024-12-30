4f233de0c826cc8d8fd13e627b2a89fb188b0ec4
InvestUz <invest.abdusattorov@gmail.com>
2024-12-30 21:29:03 +0500

    fields has been added startin login buttin

--- lib/components/login_form.dart
+++ lib/components/login_form.dart
@@ -1,3 +1,4 @@
+import 'package:doctor_app/utils/config.dart';
 import 'package:flutter/material.dart';
 
 class LoginForm extends StatefulWidget {
@@ -8,10 +9,59 @@ class LoginForm extends StatefulWidget {
 }
 
 class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool obesecurePass = true;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('form simple text'),
-    );
+    return Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            TextFormField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              cursorColor: Config.primaryColor,
+              decoration: const InputDecoration(
+                hintText: 'email address',
+                labelText: 'email',
+                alignLabelWithHint: true,
+                prefixIcon: Icon(Icons.email_outlined),
+                prefixIconColor: Config.primaryColor,
+              ),
+            ),
+            Config.spaceSmall,
+            TextFormField(
+              controller: _passwordController,
+              keyboardType: TextInputType.visiblePassword,
+              cursorColor: Config.primaryColor,
+              decoration: InputDecoration(
+                  hintText: 'password code',
+                  labelText: 'password',
+                  alignLabelWithHint: true,
+                  prefixIcon: Icon(Icons.lock_outline),
+                  prefixIconColor: Config.primaryColor,
+                  suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          obesecurePass = !obesecurePass;
+                        });
+                      },
+                      icon: obesecurePass
+                          ? const Icon(
+                              Icons.visibility_off_outlined,
+                              color: Colors.black38,
+                            )
+                          : const Icon(
+                              Icons.visibility_outlined,
+                              color: Config.primaryColor,
+                            ))),
+            ),
+            Config.spaceSmall,
+          ],
+        ));
   }
 }
