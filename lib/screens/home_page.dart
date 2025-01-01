06221dcbe0b2a7f195df5364dd219591dd18af81
InvestUz <invest.abdusattorov@gmail.com>
2025-01-01 15:23:11 +0500

    categories has been fetched using map

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:doctor_app/utils/config.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,11 +10,20 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  List<Map<String, dynamic>> medCat = [
+    {"icon": FontAwesomeIcons.userDoctor, "category": "General"},
+    {"icon": FontAwesomeIcons.heartPulse, "category": "Cardiology"},
+    {"icon": FontAwesomeIcons.lungs, "category": "Respirations"},
+    {"icon": FontAwesomeIcons.hand, "category": "Dermatology"},
+    {"icon": FontAwesomeIcons.personPregnant, "category": "Gynecology"},
+    {"icon": FontAwesomeIcons.teeth, "category": "Dental"},
+  ];
+
   @override
   Widget build(BuildContext context) {
     Config().init(context);
 
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
         padding: EdgeInsets.symmetric(
           horizontal: 15,
@@ -40,7 +50,61 @@ class HomePageState extends State<HomePage> {
                     backgroundImage: AssetImage('assets/profile1.jpg'),
                   ))
                 ],
-              )
+              ),
+              Config.spaceSmall,
+              //category listing
+              const Text(
+                'Category',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              Config.spaceSmall,
+              // fetch categories
+              SizedBox(
+                height: Config.heightSize * 0.05,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: List<Widget>.generate(medCat.length, (index) {
+                    return Card(
+                      margin: const EdgeInsets.only(right: 20),
+                      color: Config.primaryColor,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 15, vertical: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            FaIcon(
+                              medCat[index]['icon'],
+                              color: Colors.white,
+                            ),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            Text(
+                              medCat[index]['category'],
+                              style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
+                ),
+              ),
+              Config.spaceSmall,
+              const Text(
+                'Appintment Today',
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              Config.spaceSmall,
             ],
           ),
         ),
