986addc0b5e0b6a61f63baf902ece95f7c840420
InvestUz <invest.abdusattorov@gmail.com>
2025-01-01 17:55:53 +0500

    working with upgrading cards

--- lib/components/appointment_card.dart
+++ lib/components/appointment_card.dart
@@ -17,42 +17,126 @@ class _AppointmentCardState extends State<AppointmentCard> {
         color: Config.primaryColor,
         borderRadius: BorderRadius.circular(10),
       ),
-      child: const Material(
+      child: Material(
         color: Colors.transparent,
         child: Padding(
-          padding: EdgeInsets.all(20),
+          padding: const EdgeInsets.all(20),
           child: Column(
             children: <Widget>[
-              CircleAvatar(
-                backgroundImage: AssetImage('assets/doctor_1.jpg'),
+              const Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage('assets/doctor_1.jpg'),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'A Abdusattorov',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text(
+                        'Dental',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
               ),
-              SizedBox(
-                width: 10,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    'A Abdusattorov',
-                    style: TextStyle(
-                      color: Colors.white,
+              Config.spaceSmall,
+              const ScheduleCard(),
+              Config.spaceSmall,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                      ),
+                      child: const Text(
+                        'Cancel',
+                        style: TextStyle(color: Colors.white),
+                      ),
                     ),
                   ),
-                   SizedBox(
-                    height: 2,
-                  ),
-                   Text(
-                    'Dental',
-                    style: TextStyle(
-                      color: Colors.black,
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                      ),
+                      child: const Text(
+                        'Completed',
+                        style: TextStyle(color: Colors.white),
+                      ),
                     ),
-                  ),
+                  )
                 ],
               )
+              // schedule info
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class ScheduleCard extends StatelessWidget {
+  const ScheduleCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      width: double.infinity,
+      padding: const EdgeInsets.all(20),
+      child: const Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.calendar_today,
+            color: Colors.white,
+            size: 15,
+          ),
+          SizedBox(width: 5),
+          Text(
+            'monday 11/12/2022',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(width: 20),
+          Icon(
+            Icons.access_alarm,
+            color: Colors.white,
+            size: 17,
+          ),
+          SizedBox(width: 5),
+          Flexible(
+              child: Text(
+            '2:00 PM',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ))
+        ],
       ),
     );
   }
